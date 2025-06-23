b043347b9c2c3bb9c838934d6b732a5ec7b351d3
kshitijghodekar <kshitijghodekar07@gmail.com>
2025-06-23 18:32:06 +0100

    added drag in mobile view

--- src/geoApp/GeoApp.tsx
+++ src/geoApp/GeoApp.tsx
@@ -6,6 +6,7 @@ import {
   DndContext,
   closestCenter,
   PointerSensor,
+  TouchSensor,
   useSensor,
   useSensors,
 } from '@dnd-kit/core';
@@ -59,7 +60,15 @@ const GeoApp: React.FC = () => {
   };
 
   const sensors = useSensors(
-    useSensor(PointerSensor, { activationConstraint: { distance: 5 } })
+    useSensor(PointerSensor, {
+      activationConstraint: { distance: 5 },
+    }),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 150,
+        tolerance: 5,
+      },
+    })
   );
 
   const handleDragEnd = (event: any) => {
