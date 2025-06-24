9dc779b36e0a2b688d37b26610e209e81d049b5c
kshitijghodekar <kshitijghodekar07@gmail.com>
2025-06-24 14:01:53 +0100

    handle webpage analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import GeoApp from './geoApp/GeoApp.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <GeoApp />
+      <Analytics />
     </div>
   );
 }
